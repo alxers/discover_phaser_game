48a51752eeb81d4fa2a68a915c3ca2cb4ddce785
Alexey Ershov <alex4rom@gmail.com>
2014-08-09 16:37:25 +0200

    Add walls

--- main.js
+++ main.js
@@ -19,10 +19,19 @@ var mainState = {
         
         // Add cursor
         this.cursor = game.input.keyboard.createCursorKeys();
+        
+        // Create world
+        this.createWorld();
     },
     
     update: function () {
+        // Player and walls should collide
+        game.physics.arcade.collide(this.player, this.walls);
+        
         this.movePlayer();
+        if (!this.player.inWorld) {
+            this.playerDie();
+        }
     },
     
     movePlayer: function () {
@@ -40,6 +49,32 @@ var mainState = {
             // Jump
             this.player.body.velocity.y = -320;
         }
+    },
+    
+    createWorld: function () {
+        // Add walls
+        this.walls = game.add.group();
+        this.walls.enableBody = true;
+        
+        game.add.sprite(0, 0, 'wallV', 0, this.walls); // Left
+        game.add.sprite(480, 0, 'wallV', 0, this.walls); // Right
+        game.add.sprite(0, 0, 'wallH', 0, this.walls); // Top left
+        game.add.sprite(0, 320, 'wallH', 0, this.walls); // Top right
+        game.add.sprite(0, 3200, 'wallH', 0, this.walls); // Bottom left
+        game.add.sprite(300, 320, 'wallH', 0, this.walls); // Bottom right
+        game.add.sprite(-100, 160, 'wallH', 0, this.walls); // Middle left
+        game.add.sprite(400, 160, 'wallH', 0, this.walls); // Middle right
+        
+        var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls);
+        middleTop.scale.setTo(1.5, 1);
+        var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls);
+        middleBottom.scale.setTo(1.5, 1);
+        
+        this.walls.setAll('body.immovable', true);
+    },
+    
+    playerDie: function () {
+        game.state.start('main');
     }
 };
 
