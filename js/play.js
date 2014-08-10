3b5ffcc2ac75f1c598fde28867532dc5816d1d97
Alexey Ershov <alex4rom@gmail.com>
2014-08-10 11:21:31 +0200

    Add animation

--- js/play.js
+++ js/play.js
@@ -5,6 +5,8 @@ var playState = {
         // Add player
         this.player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
         this.player.anchor.setTo(0.5, 0.5);
+        this.player.animations.add('right', [1, 2], 8, true);
+        this.player.animations.add('left', [3, 4], 8, true);
         
         game.physics.arcade.enable(this.player);
         
@@ -34,7 +36,10 @@ var playState = {
         
         game.time.events.loop(2200, this.addEnemy, this);
         
-        
+        // Add sounds
+        this.jumpSound = game.add.audio('jump');
+        this.coinSound = game.add.audio('coin');
+        this.deadSound = game.add.audio('dead');
     },
     
     update: function () {
@@ -55,16 +60,21 @@ var playState = {
         if (this.cursor.left.isDown) {
             // Move player to the left
             this.player.body.velocity.x = -200;
+            this.player.animations.play('left');
         } else if (this.cursor.right.isDown) {
             // Move player to the right
             this.player.body.velocity.x = 200;
+            this.player.animations.play('right');
         } else {
             this.player.body.velocity.x = 0;
+            this.player.animations.stop();
+            this.player.frame = 0; // Show first frame
         }
         
         if (this.cursor.up.isDown && this.player.body.touching.down) {
             // Jump
             this.player.body.velocity.y = -320;
+            this.jumpSound.play();
         }
     },
     
@@ -91,6 +101,7 @@ var playState = {
     },
     
     playerDie: function () {
+        this.deadSound.play();
         game.state.start('menu');
     },
     
@@ -98,6 +109,7 @@ var playState = {
         game.global.score += 5;
         this.scoreLabel.text = 'score: ' + game.global.score;
         this.updateCoinPosition();
+        this.coinSound.play();
     },
     
     updateCoinPosition: function () {
